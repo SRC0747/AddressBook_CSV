0abe8fcb045c09351758f6eb1b7b7a5458f6dadc
SRC0747 <RjSrc@0747>
2021-09-09 13:24:53 +0530

    added: Dictionary Storing Contact Details.

--- src/test/java/com/addressbookCSV/AddressBookTest.java
+++ src/test/java/com/addressbookCSV/AddressBookTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 //import junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
+import java.io.Console;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class AddressBookTest {
     public static void main(String[] main(){
@@ -14,6 +18,25 @@ class AddressBookTest {
         details.add(new MultipleDetail("Abhishek""Baguihati","Kolkata","WestBengal","7044589936","abhi37@gmail.com"));
         details.add(new MultipleDetail("Sampriti","Belepole","Howrah","WestBengal","7044585321","srcsrk0747@gmail.com"));
         details.stream().filter(details -> details.city == "Kolkata" && details -> details.state == "WestBengal").forEach(details -> System.out.println(details.name));
+
+        //Dictionary of City and Person.
+        HashTable <City,Person> mydict_1 = new  Hashtable<City,Person>();
+        //put method() to print output.
+        mydict_1.put("Kolkata","Rakesh");
+        mydict_1.put("Howrah","Sampriti");
+        //get method() to take input.
+        System.out.println("\nPerson at Kolkata is:"+mydict_1.get("Kolkata"));
+        System.out.println("\nPerson at Howrah is:"+mydict_1.get("Howrah"));
+
+
+        //Dictionary of State and Person.
+        HashTable <State,Person> mydict_1 = new  Hashtable<State, Person>();
+        //put method() to print output.
+        mydict_1.put("West Bengal","Rakesh");
+        mydict_1.put(Mumbai","Sampriti");
+                //get method() to take input.
+                System.out.println("\n Person at West Bengal is:"+mydict_1.get("West Bengal"));
+        System.out.println("\n Person at Mumbai is:"+mydict_1.get("Mumbai"));
     }
 
     @Test
