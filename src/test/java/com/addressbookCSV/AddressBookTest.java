a6efb67b8e6c343af6be9eab92eaa43cbf6a5f26
SRC0747 <RjSrc@0747>
2021-09-09 13:18:33 +0530

    added: Using JavaStream to add MultipleDetails.

--- src/test/java/com/addressbookCSV/AddressBookTest.java
+++ src/test/java/com/addressbookCSV/AddressBookTest.java
@@ -1,10 +1,20 @@
 package com.addressbookCSV;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 //import junit.jupiter.api.Assertions.*;
 
 class AddressBookTest {
-    AddressBookTest obj = new AddressBookTest();
+    public static void main(String[] main(){
+        AddressBookTest obj = new AddressBookTest();
+        ArrayList<MultipleDetail> details = new ArrayList<MultipleDetail>();
+        //Adding Details
+        details.add(new MultipleDetail("Rakesh Jaiswal","Amhastreet","Kolkata","WestBengal","9632457896","rjsrc@gmail.com"));
+        details.add(new MultipleDetail("Abhishek""Baguihati","Kolkata","WestBengal","7044589936","abhi37@gmail.com"));
+        details.add(new MultipleDetail("Sampriti","Belepole","Howrah","WestBengal","7044585321","srcsrk0747@gmail.com"));
+        details.stream().filter(details -> details.city == "Kolkata" && details -> details.state == "WestBengal").forEach(details -> System.out.println(details.name));
+    }
 
     @Test
     public void toEnter_PersonDetail(){
